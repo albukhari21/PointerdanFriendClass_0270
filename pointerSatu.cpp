8391a6afb47c7552238b57cde8e583aa0deaef31
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-06 10:59:09 +0700

    menambahkan class mahasiswa

---
+++ pointerSatu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa{
+    public:
+    int nim;
+    void showNim(){
+        cout << "No Induk = " << nim <<endl;
+    }
+};
+
+int main (){
+
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+}
